fb4d3a2dd453e778021cb6dab26f4bab84a8bfc4
AleixosFernando <fernandoaleixos@iesmre.com>
2022-03-01 14:00:28 +0100

    Creació de Actividades i Juego de Cartas - 01/02/2022

--- src/juegoDeCartas/Carta.java
+++ src/juegoDeCartas/Carta.java
@@ -1,5 +1,16 @@
 package juegoDeCartas;
 
 public class Carta {
+    public static final int OROS = 0;
+    public static final int COPAS = 1;
+    public static final int ESPADAS = 2;
+    public static final int BASTOS = 3;
 
+    private int palo;
+    private int valor;
+
+    public Carta() {
+        this.palo = (int)((Math.random() * 4) + 1);
+        this.valor = (int)((Math.random() * 12) + 1);
+    }
 }
