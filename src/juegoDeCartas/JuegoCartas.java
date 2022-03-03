b8c6ca3b029aebb9f222bddde863b0cc7e1478d0
FernandoAleixos <fernanaleixos0204@gmail.com>
2022-03-03 21:49:45 +0100

    Creació de clase JuegoCartas.java - 03/03/2022

---
+++ src/juegoDeCartas/JuegoCartas.java
@@ -0,0 +1,16 @@
+package juegoDeCartas;
+
+public class JuegoCartas {
+
+    //Un método de clase (estático) public static int ganadora( Carta c1, Carta
+    //c2, int triunfo) que dados dos objetos Carta y un número entero
+    //representando el palo de triunfo (o palo ganador), determine cuál es la carta
+    //ganadora. El método debe devolver 0 si las dos cartas son iguales. En caso
+    //contrario, devolverá -1 cuando la primera carta es la ganadora y 1 si la segunda
+    //carta es la ganadora.
+    public static int ganadora(Carta c1, Carta c2, int triunfo) {
+
+        
+        return ;
+    }
+}
