b9ff73aaf8aff6bf80aba301bb94b195e661272e
AleixosFernando <fernandoaleixos@iesmre.com>
2022-03-14 13:07:28 +0100

    Fechas - 14/03/2022

--- src/fechas/Act_2_7.java
+++ src/fechas/Act_2_7.java
@@ -1,5 +1,41 @@
 package fechas;
 
-public class Act_2_7 {
+import java.time.LocalDate;
+import java.util.GregorianCalendar;
+import java.util.Scanner;
 
+public class Act_2_7 {
+    //Introduce un dia para la fecha: 29
+    //Introduce un mes para la fecha: 2
+    //Introduce un año para la fecha: 2022
+    //LA FECHA ES INCORRECTA
+
+    //Introduce un dia para la fecha: 29
+    //Introduce un mes para la fecha: 2
+    //Introduce un año para la fecha: 2020
+    //LA FECHA ES CORRECTA
+
+    public static void main(String[] args) {
+        Scanner tec = new Scanner(System.in);
+        GregorianCalendar calendari = new GregorianCalendar();
+
+        System.out.print("Introduce un día para la fecha: ");
+        int dia = tec.nextInt();
+        tec.nextLine();
+
+        System.out.print("Introduce un mes para la fecha: ");
+        int mes = tec.nextInt();
+        tec.nextLine();
+
+        System.out.print("Introduce un año para la fecha: ");
+        int anyo = tec.nextInt();
+        tec.nextLine();
+
+        LocalDate fecha = LocalDate.of(anyo, mes, dia);
+
+        if (calendari.isLeapYear(anyo)) {
+
+        }
+
+    }
 }
